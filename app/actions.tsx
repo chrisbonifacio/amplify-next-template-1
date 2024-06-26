f2236ab6c9f43dadb59ac87d0c63d13c546cae62
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-06-26 14:35:49 -0400

    update repro

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,11 +1,26 @@
 "use server";
 
-import { cookieBasedClient } from "@/app/utils/amplifyServerUtils";
+import { Schema } from "@/amplify/data/resource";
+import {
+  cookieBasedClient,
+  reqResBasedClient,
+} from "@/app/utils/amplifyServerUtils";
+import { generateClient } from "aws-amplify/api";
 
+const client = generateClient<Schema>();
 export async function createTodo(formData: FormData) {
   const content = formData.get("content") as string;
 
-  const { data } = await cookieBasedClient.models.Todo.create({
+  const { data: cookieData } = await cookieBasedClient.models.Todo.create({
+    content,
+  });
+
+  const { data: reqResData } = await reqResBasedClient.models.Todo.create(
+    {},
+    { content }
+  );
+
+  const { data } = await client.models.Todo.create({
     content,
   });
 
--- app/utils/amplifyServerUtils.ts
+++ app/utils/amplifyServerUtils.ts
@@ -1,9 +1,16 @@
 import { cookies } from "next/headers";
-import { generateServerClientUsingCookies } from "@aws-amplify/adapter-nextjs/data";
+import {
+  generateServerClientUsingCookies,
+  generateServerClientUsingReqRes,
+} from "@aws-amplify/adapter-nextjs/data";
 import type { Schema } from "@/amplify/data/resource";
 import outputs from "@/amplify_outputs.json";
 
 export const cookieBasedClient = generateServerClientUsingCookies<Schema>({
   config: outputs,
   cookies,
+});
+
+export const reqResBasedClient = generateServerClientUsingReqRes<Schema>({
+  config: outputs,
 });
